8b0126e752d3b475b7fc5da967590b589511d46c
Munthir <munthir.muh@gmail.com>
2023-05-24 00:44:50 +0300

    change parameters

--- NationalIdExtractorPresentation/Program.cs
+++ NationalIdExtractorPresentation/Program.cs
@@ -3,8 +3,8 @@ using NationalIdExtraction.Providers;
 using NationalIdExtractorPresentation;
 
 //using Default Resource Provider
-Console.WriteLine(new NationalIdExtractor().Extract("31011202101545"));
+Console.WriteLine(new NationalIdExtractor().Extract("31011208801846"));
 Console.WriteLine();
 
 //using custome Resource provider
-Console.WriteLine(new NationalIdExtractor(new NationalIdResourcesArabicProvider()).Extract("31013202101545", ignoreLegalAge:true, legalAge:15));
+Console.WriteLine(new NationalIdExtractor(new NationalIdResourcesArabicProvider()).Extract("310132001743", ignoreLegalAge:true, legalAge:15));
